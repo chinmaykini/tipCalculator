276024f7af8b372ee6d93697ed4d621e85c06df6
Chinmay Kini <kini-yahoo.inc.com>
2015-01-18 17:26:21 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Chinmay Kini on 1/18/15.
+//  Copyright (c) 2015 Chinmay Kini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
